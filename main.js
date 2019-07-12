586fbe287b0d92b2e4d8f711b72ad094724346c2
milan.zivanov <milan.zivanov@gmail.com>
2019-07-12 11:36:12 +0200

    magmell task

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 
-// get all box rows
-var arr = document.querySelectorAll('.box');
-// loop over box array
-arr.forEach( function(el){
-  // generate random number
-  var x = Math.floor((Math.random() * 9) + 1);
-  // pass that number to element node
-  el.innerText = x;
-});
+ 
+$('.main').empty();
+
+for (var a = [1, 2, 3, 4, 5, 6, 7, 8, 9], i = a.length; i--; ) {
+    var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+    $('.main').append('<div class="box">' + random + '</div>');
+}
+
+// more examples
+// $('.box').each(function () {
+//   var x = Math.floor((Math.random() * 9) + 1);
+//   $(this).text(x)
+// });
+
+// setInterval(function() {
+//   var number = 1 + Math.floor(Math.random() * 9);
+//   $('.box').text(number);
+// },
+// 1000); // every 1 second
+
+
 
 
 
